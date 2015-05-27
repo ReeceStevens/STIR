6ac23c35050c5134ac4e218f3c5b03eb80676b64
ReeceStevens <reecestevens@utexas.edu>
2015-05-27 17:10:53 -0500

    Working image viewer in python

--- analysis/scratch.py
+++ analysis/scratch.py
@@ -3,12 +3,24 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
+# Load scan from file
 img = nib.load('/Users/reecestevens/Downloads/output.nii')
 data = img.get_data()
+newdata = np.zeros([24,256,256])
 
+# Reshape image data for ease of use
 for x in range(0, len(data)):
     for y in range(0,len(data[0])):
         for z in range(0,len(data[0,0])):
             newdata[z,x,y] = data[x,y,z]
 
+# Display all slices in the file
+i = 1;
+for image in newdata:
+    plt.subplot(4,6,i)
+    plt.imshow(image, 'gray')
+    plt.title('Slice ' + str(i))
+    plt.xticks([]), plt.yticks([])
+    i += 1;
+plt.show()
 
